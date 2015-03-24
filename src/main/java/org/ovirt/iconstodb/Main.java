fc0b66a90e46165dc37633aeaf6c4df0f300310d
Jakub Niedermertl <jniederm@redhat.com>
2015-03-24 12:38:27 +0100

    generator for table vm_icons(id, data_url, default_for_os, is_small)

--- src/main/java/org/ovirt/iconstodb/Main.java
+++ src/main/java/org/ovirt/iconstodb/Main.java
@@ -2,8 +2,10 @@ package org.ovirt.iconstodb;
 
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -18,29 +20,42 @@ import java.util.stream.StreamSupport;
 
 public class Main {
 
-    private static final String HOME = System.getProperty("user.home");
-    private static final Path INPUT_DIR = Paths.get(HOME + "/src/ovirt-engine/frontend/webadmin/modules/userportal-gwtp/src/main/resources/org/ovirt/engine/ui/userportal/images/os/large");
+    private static final String HOME_DIR = System.getProperty("user.home");
+    private static final Path LARGE_ICONS_INPUT_DIR = Paths.get(HOME_DIR + "/src/ovirt-engine/frontend/webadmin/modules/userportal-gwtp/src/main/resources/org/ovirt/engine/ui/userportal/images/os/large");
+    private static final Path SMALL_ICONS_INPUT_DIR = Paths.get(HOME_DIR + "/src/ovirt-engine/frontend/webadmin/modules/userportal-gwtp/src/main/resources/org/ovirt/engine/ui/userportal/images/os");
 
     private static Map<String, Integer> osNameToIdMap = new HashMap<>();
 
+    private static PrintStream printStream;
+
     public static void main(String[] args) throws IOException {
+        printStream = new PrintStream(new File("output.sql"));
         initOsMap();
-        final DirectoryStream<Path> paths = Files.newDirectoryStream(INPUT_DIR,
-                path -> path.toString().matches(".+\\.(png|jpg|jpeg|gif)$"));
-        StreamSupport.stream(paths.spliterator(), false)
-                .map(path -> getInsertLine(
-                        toUuid(toNameWithoutExtension(path)),
-                        pathToDataUri(path),
-                        toNameWithoutExtension(path)
-                ))
-                .forEach(item -> {
-                    System.out.println(item);
-                });
+        printDirectory(LARGE_ICONS_INPUT_DIR, false);
+        printDirectory(SMALL_ICONS_INPUT_DIR, true);
+        printStream.close();
 
     }
 
+    private static void printDirectory(Path directory, boolean isSmall) throws IOException {
+        final DirectoryStream<Path> paths = Files.newDirectoryStream(directory,
+                path -> path.toString().matches(".+\\.(png|jpg|jpeg|gif)$"));
+        StreamSupport.stream(paths.spliterator(), false)
+                .map(path -> {
+                    final String dataUri = pathToDataUri(path);
+                    final String insertLineStatement = getInsertLine(
+                            toUuid(dataUri),
+                            dataUri,
+                            toNameWithoutExtension(path),
+                            isSmall);
+                    return insertLineStatement;
+                })
+                .forEach(item -> printStream.println(item));
+        printStream.println();
+    }
+
     private static void initOsMap() throws IOException {
-        final Path configFile = Paths.get(HOME + "/src/ovirt-engine/packaging/conf/osinfo-defaults.properties");
+        final Path configFile = Paths.get(HOME_DIR + "/src/ovirt-engine/packaging/conf/osinfo-defaults.properties");
         Files.lines(configFile)
                 .filter(line -> line.matches("os\\.[^.]+\\.id\\.value.*"))
                 .forEach(line -> {
@@ -53,9 +68,14 @@ public class Main {
                 });
     }
 
-    private static String getInsertLine(String uuid, String dataUri, String osName) {
-        return "-- " + osName + " " + osNameToIdMap.get(osName) + System.lineSeparator()
-                + "INSERT INTO vm_icons(id, data_url, default_for_os) VALUES ('" + uuid + "', '" + dataUri + "', " + osNameToIdMap.get(osName) + ");";
+    private static String getInsertLine(String uuid, String dataUri, String osName, boolean isSmall) {
+        final Integer osId = osNameToIdMap.get(osName);
+        return "-- " + osName
+                + " osId=" + osId
+                + " " + (isSmall ? "small" : "large")
+                + System.lineSeparator()
+                + "INSERT INTO vm_icons(id, data_url, default_for_os, is_small) "
+                + "VALUES ('" + uuid + "', '" + dataUri + "', " + osId + ", " + String.valueOf(isSmall) + " );";
     }
 
     private static String toUuid(String str) {
